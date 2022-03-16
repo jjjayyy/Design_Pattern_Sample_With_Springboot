1ac4bbb027ce3ff892bf5cc14f9c2d81edc289fa
jjjayyy <hwinnovation@naver.com>
2022-03-16 21:00:46 +0900

    init

---
+++ src/main/java/com/example/design_pattern_sample_with_springboot/DesignPatternSampleWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.design_pattern_sample_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternSampleWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternSampleWithSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/design_pattern_sample_with_springboot/DesignPatternSampleWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.design_pattern_sample_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternSampleWithSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
